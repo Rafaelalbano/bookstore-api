249e16ae4931367c9a1a793f25d04e7567867b96
rafaelalbano <rafaelb.albano@gmail.com>
2021-05-31 23:02:33 -0300

    Classes de domínio iniciadas

---
+++ src/main/java/com/rafael/bookstore/domain/Categoria.java
@@ -0,0 +1,82 @@
+package com.rafael.bookstore.domain;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Categoria {
+	
+	private Integer Id;
+	private String nome;
+	private String descricao;
+	
+	private List<Livro> livros = new ArrayList<>();
+
+	public Categoria() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Categoria(Integer id, String nome, String descricao) {
+		super();
+		Id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Livro> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(List<Livro> livros) {
+		this.livros = livros;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+
+}
